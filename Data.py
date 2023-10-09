e483b6b1b3c9aa2457af6e07b4200fa713a3e2d1
vladvdovin27 <vladsaransk27@mail.ru>
2023-10-09 22:26:46 +0300

    New fignya

--- Data.py
+++ Data.py
@@ -1,4 +1,6 @@
 import cv2 as cv
+import pandas as pd
+import config as cf
 
 
 def read_data(filename, start_index=None, end_index=None):
@@ -9,19 +11,45 @@ def read_data(filename, start_index=None, end_index=None):
     :param end_index: Индекс конца парсинга
     :return: List[]
     """
-    pass
+    df = pd.read_csv(filename)
+
+    start = 0 if start_index is None else start_index
+    end = df.shape[0] if end_index is None else end_index
+
+    for i in range(start, end):
+        video_name = df.loc[i]['attachment_id']
+        info = (df.loc[i]['height'], df.loc[i]['width'], df.loc[i]['length'])
+
+        result = get_frames(video_name + '.mp4', df.loc[i]['begin'], df.loc[i]['end'])
 
 
-def get_frames(path2file):
+def get_frames(path2file, begin_frame, end_frame):
     """
     Функция обработки видео по кадрам
     :param path2file:
+    :param begin_frame:
+    :param end_frame:
     :return: List
     """
-    pass
+    video = cv.VideoCapture(path2file)
+
+    if not video.isOpened():
+        return []
+
+    frame_number = 1
+
+    frames_list = []
+
+    while video.isOpened():
+        fl, frame = video.read()
+
+        if begin_frame <= frame_number <= end_frame:
+            frames_list.append(cv.resize(cf.FRAME_SIZE, frame) / 255)
+
+    return process_element(frames_list)
 
 
-def process_element(element, result_size=(20, 640, 360, 3), variant='4d'):
+def process_element(element, result_size=(35, 640, 360, 3), variant='4d'):
     """
     Функция изменения размерности данных
     :param element: Элемент для обработки
